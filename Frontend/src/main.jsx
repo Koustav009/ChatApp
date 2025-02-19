9e057075c53f977db46c5fba23e16347dfb3217f
Koustav Manna <manna98.koustav@gmail.com>
2025-02-20 00:24:12 +0530

    small changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
-import AppRoutes from "./config/routes.jsx";
+import AppRoutes from "./config/Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
